b2f49d1b3a881d010c8cb6c4e518af54535fe23e
LeoKuD <then7w@gmail.com>
2021-11-08 19:58:36 +0300

    add verstka

---
+++ src/data.jsx
@@ -0,0 +1,27 @@
+import img1 from 'assets/img/pizza1.png';
+import img2 from 'assets/img/pizza2.png';
+import img3 from 'assets/img/pizza3.png';
+import img4 from 'assets/img/pizza4.png';
+import img5 from 'assets/img/pizza4.png';
+import img6 from 'assets/img/pizza4.png';
+import img7 from 'assets/img/pizza4.png';
+import img8 from 'assets/img/pizza4.png';
+
+export const data = [
+  { pizzaName: 'Чизбургер-пицца', pizzaImg: img1 },
+  { pizzaName: 'Сырная', pizzaImg: img2 },
+  { pizzaName: 'Креветки по-азиатски', pizzaImg: img3 },
+  { pizzaName: 'Сырный цыпленок', pizzaImg: img4 },
+  { pizzaName: 'Чизбургер-пицца', pizzaImg: img5 },
+  { pizzaName: 'Сырная', pizzaImg: img6 },
+  { pizzaName: 'Креветки по-азиатски', pizzaImg: img7 },
+  { pizzaName: 'Сырный цыпленок', pizzaImg: img8 },
+];
+
+export const categories = [
+  'Мясные',
+  'Вегетарианская',
+  'Гриль',
+  'Острые',
+  'Закрытые',
+];
